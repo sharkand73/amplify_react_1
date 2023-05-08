155651eaebec2152444bbfcebb3371f571824b31
Andrew Sharkey <sharkand@hotmail.com>
2023-05-09 00:49:24 +0100

    Authenticator loads but without styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+
 
 function App() {
+  Amplify.configure(awsConfig);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        <h2>Hello World!</h2>
+      </Authenticator>
     </div>
   );
 }
